905dfe7c45963beeb05ca09a0ae5aed62c89f192
Yurii Popleta <yurka279@gmail.com>
2022-11-16 18:49:04 +0100

    Initial Commit

---
+++ NewSwiftUI/NewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftUIApp.swift
+//  NewSwiftUI
+//
+//  Created by Admin on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
